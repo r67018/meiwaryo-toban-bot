246c6041bc7fb82d480ed6e9a7cbd1d76377706c
swk67018 <swear1208a13@gmail.com>
2021-03-16 15:59:23 +0900

    Put setLastDate(), setLastGroup() and setReply() together

--- adjust.py
+++ adjust.py
@@ -15,9 +15,12 @@ def advance():
         last_group_index = bc.advanceGroup(group=today_group, index=last_group_index)
 
     now_dt = datetime.now()
-    bc.setLastInfo(month=now_dt.month, day=now_dt.day, last_group=today_group)
-    bc.setLastGroup(today_group[bc.group_per_day - 1])
-    bc.setLastDate(now_dt.year, now_dt.month, now_dt.day)
+    bc.setLastInfo(
+        year=now_dt.year,
+        month=now_dt.month,
+        day=now_dt.day,
+        group=today_group
+    )
 
 
 def retreat():
@@ -27,6 +30,9 @@ def retreat():
         last_group_index = bc.advanceGroup(group=today_group, index=last_group_index)
 
     now_dt = datetime.now()
-    bc.setLastInfo(month=now_dt.month, day=now_dt.day, last_group=today_group)
-    bc.setLastGroup(today_group[bc.group_per_day - 1])
-    bc.setLastDate(now_dt.year, now_dt.month, now_dt.day)
+    bc.setLastInfo(
+        year=now_dt.year,
+        month=now_dt.month,
+        day=now_dt.day,
+        group=today_group
+    )
