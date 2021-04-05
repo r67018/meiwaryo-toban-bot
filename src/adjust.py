4575a66c39e0d3a56cf8f19edfe82d09bab0eda9
swk67018 <swear1208a13@gmail.com>
2021-04-05 23:02:28 +0900

    Remove unnecessary parts

--- src/adjust.py
+++ src/adjust.py
@@ -11,7 +11,7 @@ reRETREAT = r'\sret$'
 def advance():
     today_group = []
     last_group_index = bc.main_group.index(bc.getLastGroup()) - bc.group_per_day + 1 % bc.group_size
-    last_group_index = bc.advanceGroup(group=today_group, index=last_group_index)
+    bc.advanceGroup(group=today_group, index=last_group_index)
 
     now_dt = datetime.now()
     bc.setLastInfo(
@@ -25,7 +25,7 @@ def advance():
 def retreat():
     today_group = []
     last_group_index = bc.main_group.index(bc.getLastGroup()) - bc.group_per_day - 1 % bc.group_size
-    last_group_index = bc.advanceGroup(group=today_group, index=last_group_index)
+    bc.advanceGroup(group=today_group, index=last_group_index)
 
     now_dt = datetime.now()
     bc.setLastInfo(
